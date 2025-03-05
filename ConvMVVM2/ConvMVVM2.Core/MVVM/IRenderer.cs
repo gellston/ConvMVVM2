38f7306f7bb949400df36b3903456d6f5d7f2ebf
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2025-03-05 12:07:01 +0900

    1. IRenderer 업데이트 2. CompositionRenderBehavior

---
+++ ConvMVVM2/ConvMVVM2.Core/MVVM/IRenderer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConvMVVM2.Core.MVVM
+{
+    public interface IRenderer
+    {
+        void OnRendering();
+    }
+}
---
+++ ConvMVVM2/ConvMVVM2.WPF/Behaviors/CompositionRenderBehavior.cs
@@ -0,0 +1,71 @@
+﻿using ConvMVVM2.Core.MVVM;
+using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ConvMVVM2.WPF.Behaviors
+{
+    public class CompositionRenderBehavior : Behavior<FrameworkElement>
+    {
+
+        #region Protected Functions
+
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+           
+        }
+
+
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            CompositionTarget.Rendering -= CompositionTarget_Rendering;
+        }
+        #endregion
+
+        #region Dependency Property
+        public static DependencyProperty RendererProperty = DependencyProperty.Register("Renderer", typeof(IRenderer), typeof(Behavior<FrameworkElement>));
+        public IRenderer Renderer
+        {
+            get => (IRenderer)GetValue(RendererProperty);
+            set => SetValue(RendererProperty, value);
+        }
+        #endregion
+
+        #region Event Handler
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            if (!(sender is Behavior<FrameworkElement> behavior)) return;
+           
+            try
+            {
+                if (Renderer != null) {
+                    Renderer.OnRendering();
+                    return;
+                }
+
+
+                if (!(AssociatedObject.DataContext is IRenderer render)) return;
+                render.OnRendering();
+          
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        #endregion
+    }
+}
