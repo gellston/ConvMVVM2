58e4977dc32e0cf66ab0e71bc6cb388b48829f46
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2025-06-09 16:18:25 +0900

    1. SelectedImtesBehaivor 업데이트

---
+++ ConvMVVM2/ConvMVVM2.WPF/Behaviors/SelectedItemsBehaivor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace ConvMVVM2.WPF.Behaviors
+{
+    public class SelectedItemsBehavior : Behavior<ListBox>
+    {
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.Register(
+                nameof(SelectedItems),
+                typeof(IList),
+                typeof(SelectedItemsBehavior),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public IList SelectedItems
+        {
+            get => (IList)GetValue(SelectedItemsProperty);
+            set => SetValue(SelectedItemsProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            if (AssociatedObject != null)
+            {
+                AssociatedObject.SelectionChanged += OnSelectionChanged;
+
+                if (AssociatedObject.SelectedItems != null)
+                {
+                    SelectedItems = new ArrayList(AssociatedObject.SelectedItems);
+                }
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (AssociatedObject != null)
+            {
+                AssociatedObject.SelectionChanged -= OnSelectionChanged;
+            }
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AssociatedObject != null)
+            {
+                SelectedItems = new ArrayList(AssociatedObject.SelectedItems);
+            }
+        }
+    }
+}
