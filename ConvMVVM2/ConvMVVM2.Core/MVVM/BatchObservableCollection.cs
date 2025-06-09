39b583dd678c667e79b2b78bee1e9b6612d5f9b0
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2025-06-09 21:02:02 +0900

    1. Progress 업데이트 2. BatchObservableCollection 업데이트

---
+++ ConvMVVM2/ConvMVVM2.Core/MVVM/BatchObservableCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace ConvMVVM2.Core.MVVM
+{
+    public class BatchObservableCollection<T> : ObservableCollection<T>
+    {
+        public void Update(IEnumerable<T> collection)
+        {
+            base.Items.Clear();
+
+            foreach (var item in collection)
+            {
+                base.Items.Add(item);
+            }
+
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public void Update()
+        {
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+    }
+}
---
+++ ConvMVVM2/ConvMVVM2.Core/MVVM/Progress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConvMVVM2.Core.MVVM
+{
+    public class Progress<T> : IProgress<T>
+    {
+        private readonly SynchronizationContext _syncContext;
+        private readonly Action<T> _handler;
+
+        public Progress(Action<T> handler)
+        {
+            _syncContext = SynchronizationContext.Current;
+            _handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        public void Report(T value)
+        {
+            if (_syncContext != null)
+                _syncContext.Post(_ => _handler((T)_), value);
+            else
+                _handler(value);
+        }
+    }
+}
