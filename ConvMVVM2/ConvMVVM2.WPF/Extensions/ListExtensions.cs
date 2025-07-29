a80b9787d0f277a1cc441bcac396e34be0c7bc27
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2025-07-29 10:49:32 +0900

    1. ListExtension 추가

---
+++ ConvMVVM2/ConvMVVM2.WPF/Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvMVVM2.WPF.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void AddRange<T>(this List<T> list, IEnumerable<T> items)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+
+            foreach (var item in items)
+            {
+                list.Add(item);
+            }
+        }
+    }
+}
