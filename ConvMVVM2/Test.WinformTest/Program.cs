9fad56f6d10fdf72c899497d1d068bde2ebdc84a
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2025-12-08 13:30:50 +0900

    1. Localizer 업데이트

---
+++ ConvMVVM2/Test.WinformTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Test.WinformTest.Presenter;
+using Test.WinformTest.View;
+
+namespace Test.WinformTest
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+
+            var host = ConvMVVM2.Winform.Host.ConvMVVM2Host.CreateHost<BootStrapper>(args, "App");
+            host.Build()
+                .RunApp<MainPresenter>();
+
+
+
+            //Application.Run(new MainView());
+        }
+    }
+}
